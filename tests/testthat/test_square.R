e7a17ba72fc57a2e68afceb93664325a8fc8f9a1
xinyaofan <xinyao.fan95@gmail.com>
2017-11-25 22:52:00 -0800

    1

--- tests/testthat/test_square.R
+++
@@ -1,5 +0,0 @@
-context("Squaring non-numerics")
-test_that("Logicals automatically convert to numeric.", {
-	logic_vec <- c(TRUE, TRUE, FALSE)
-	expect_identical(square(logic_vec), logic_vec^2)
-})
--- tests/testthat/test_square2.R
+++
@@ -1,6 +0,0 @@
-context("Squaring non-numerics2")
-test_that("At least numeric values work.", {
-	num_vec <- c(0, -4.6, 3.4)
-	expect_identical(square(numeric(0)), numeric(0))
-	expect_identical(square(num_vec), num_vec^2)
-})
